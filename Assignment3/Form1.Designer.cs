0870ef91b01c46a0849d7918b7fb421e22ffec2f
jesus <67379392+jayvelasquez@users.noreply.github.com>
2021-01-29 16:46:17 -0700

    v1

--- Assignment3/Form1.Designer.cs
+++ Assignment3/Form1.Designer.cs
@@ -29,13 +29,246 @@ namespace Assignment3
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.countsBox = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.noAssignInput = new System.Windows.Forms.TextBox();
+            this.noStudInput = new System.Windows.Forms.TextBox();
+            this.noAssignLabel = new System.Windows.Forms.Label();
+            this.noStudentsLabel = new System.Windows.Forms.Label();
+            this.testLabel = new System.Windows.Forms.Label();
+            this.testButton = new System.Windows.Forms.Button();
+            this.testLabel2 = new System.Windows.Forms.Label();
+            this.studentInfoBox = new System.Windows.Forms.GroupBox();
+            this.studentNameLabel = new System.Windows.Forms.Label();
+            this.nameInput = new System.Windows.Forms.TextBox();
+            this.saveNameButton = new System.Windows.Forms.Button();
+            this.navBox = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.prevStudButton = new System.Windows.Forms.Button();
+            this.nextStudButton = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.countsBox.SuspendLayout();
+            this.studentInfoBox.SuspendLayout();
+            this.navBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // countsBox
+            // 
+            this.countsBox.Controls.Add(this.button1);
+            this.countsBox.Controls.Add(this.noAssignInput);
+            this.countsBox.Controls.Add(this.noStudInput);
+            this.countsBox.Controls.Add(this.noAssignLabel);
+            this.countsBox.Controls.Add(this.noStudentsLabel);
+            this.countsBox.Location = new System.Drawing.Point(13, 13);
+            this.countsBox.Name = "countsBox";
+            this.countsBox.Size = new System.Drawing.Size(558, 158);
+            this.countsBox.TabIndex = 0;
+            this.countsBox.TabStop = false;
+            this.countsBox.Text = "Counts";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(350, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 51);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Submit Counts";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // noAssignInput
+            // 
+            this.noAssignInput.Location = new System.Drawing.Point(224, 95);
+            this.noAssignInput.Name = "noAssignInput";
+            this.noAssignInput.Size = new System.Drawing.Size(100, 26);
+            this.noAssignInput.TabIndex = 3;
+            // 
+            // noStudInput
+            // 
+            this.noStudInput.Location = new System.Drawing.Point(224, 52);
+            this.noStudInput.Name = "noStudInput";
+            this.noStudInput.Size = new System.Drawing.Size(100, 26);
+            this.noStudInput.TabIndex = 2;
+            // 
+            // noAssignLabel
+            // 
+            this.noAssignLabel.AutoSize = true;
+            this.noAssignLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.noAssignLabel.Location = new System.Drawing.Point(15, 101);
+            this.noAssignLabel.Name = "noAssignLabel";
+            this.noAssignLabel.Size = new System.Drawing.Size(203, 20);
+            this.noAssignLabel.TabIndex = 1;
+            this.noAssignLabel.Text = "Number of assignments:";
+            // 
+            // noStudentsLabel
+            // 
+            this.noStudentsLabel.AutoSize = true;
+            this.noStudentsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.noStudentsLabel.Location = new System.Drawing.Point(46, 58);
+            this.noStudentsLabel.Name = "noStudentsLabel";
+            this.noStudentsLabel.Size = new System.Drawing.Size(172, 20);
+            this.noStudentsLabel.TabIndex = 0;
+            this.noStudentsLabel.Text = "Number of students:";
+            // 
+            // testLabel
+            // 
+            this.testLabel.AutoSize = true;
+            this.testLabel.Location = new System.Drawing.Point(140, 174);
+            this.testLabel.Name = "testLabel";
+            this.testLabel.Size = new System.Drawing.Size(14, 20);
+            this.testLabel.TabIndex = 1;
+            this.testLabel.Text = "-";
+            // 
+            // testButton
+            // 
+            this.testButton.Location = new System.Drawing.Point(161, 170);
+            this.testButton.Name = "testButton";
+            this.testButton.Size = new System.Drawing.Size(104, 35);
+            this.testButton.TabIndex = 2;
+            this.testButton.Text = "Test";
+            this.testButton.UseVisualStyleBackColor = true;
+            this.testButton.Click += new System.EventHandler(this.testButton_Click);
+            // 
+            // testLabel2
+            // 
+            this.testLabel2.AutoSize = true;
+            this.testLabel2.Location = new System.Drawing.Point(140, 212);
+            this.testLabel2.Name = "testLabel2";
+            this.testLabel2.Size = new System.Drawing.Size(14, 20);
+            this.testLabel2.TabIndex = 3;
+            this.testLabel2.Text = "-";
+            // 
+            // studentInfoBox
+            // 
+            this.studentInfoBox.Controls.Add(this.saveNameButton);
+            this.studentInfoBox.Controls.Add(this.nameInput);
+            this.studentInfoBox.Controls.Add(this.studentNameLabel);
+            this.studentInfoBox.Location = new System.Drawing.Point(13, 321);
+            this.studentInfoBox.Name = "studentInfoBox";
+            this.studentInfoBox.Size = new System.Drawing.Size(529, 75);
+            this.studentInfoBox.TabIndex = 4;
+            this.studentInfoBox.TabStop = false;
+            this.studentInfoBox.Text = "Student Info";
+            // 
+            // studentNameLabel
+            // 
+            this.studentNameLabel.AutoSize = true;
+            this.studentNameLabel.Location = new System.Drawing.Point(19, 35);
+            this.studentNameLabel.Name = "studentNameLabel";
+            this.studentNameLabel.Size = new System.Drawing.Size(14, 20);
+            this.studentNameLabel.TabIndex = 0;
+            this.studentNameLabel.Text = "-";
+            // 
+            // nameInput
+            // 
+            this.nameInput.Location = new System.Drawing.Point(118, 32);
+            this.nameInput.Name = "nameInput";
+            this.nameInput.Size = new System.Drawing.Size(283, 26);
+            this.nameInput.TabIndex = 1;
+            // 
+            // saveNameButton
+            // 
+            this.saveNameButton.Location = new System.Drawing.Point(407, 24);
+            this.saveNameButton.Name = "saveNameButton";
+            this.saveNameButton.Size = new System.Drawing.Size(116, 43);
+            this.saveNameButton.TabIndex = 2;
+            this.saveNameButton.Text = "Save Name";
+            this.saveNameButton.UseVisualStyleBackColor = true;
+            // 
+            // navBox
+            // 
+            this.navBox.Controls.Add(this.button5);
+            this.navBox.Controls.Add(this.nextStudButton);
+            this.navBox.Controls.Add(this.prevStudButton);
+            this.navBox.Controls.Add(this.button2);
+            this.navBox.Location = new System.Drawing.Point(13, 243);
+            this.navBox.Name = "navBox";
+            this.navBox.Size = new System.Drawing.Size(558, 72);
+            this.navBox.TabIndex = 5;
+            this.navBox.TabStop = false;
+            this.navBox.Text = "Navigate";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(7, 26);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 40);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // prevStudButton
+            // 
+            this.prevStudButton.Location = new System.Drawing.Point(148, 25);
+            this.prevStudButton.Name = "prevStudButton";
+            this.prevStudButton.Size = new System.Drawing.Size(138, 40);
+            this.prevStudButton.TabIndex = 1;
+            this.prevStudButton.Text = "< Prev Student";
+            this.prevStudButton.UseVisualStyleBackColor = true;
+            this.prevStudButton.Click += new System.EventHandler(this.prevStudButton_Click);
+            // 
+            // nextStudButton
+            // 
+            this.nextStudButton.Location = new System.Drawing.Point(292, 25);
+            this.nextStudButton.Name = "nextStudButton";
+            this.nextStudButton.Size = new System.Drawing.Size(139, 40);
+            this.nextStudButton.TabIndex = 2;
+            this.nextStudButton.Text = "Next Student >";
+            this.nextStudButton.UseVisualStyleBackColor = true;
+            this.nextStudButton.Click += new System.EventHandler(this.nextStudButton_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(437, 26);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(115, 40);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(852, 450);
+            this.Controls.Add(this.navBox);
+            this.Controls.Add(this.studentInfoBox);
+            this.Controls.Add(this.testLabel2);
+            this.Controls.Add(this.testButton);
+            this.Controls.Add(this.testLabel);
+            this.Controls.Add(this.countsBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.countsBox.ResumeLayout(false);
+            this.countsBox.PerformLayout();
+            this.studentInfoBox.ResumeLayout(false);
+            this.studentInfoBox.PerformLayout();
+            this.navBox.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox countsBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox noAssignInput;
+        private System.Windows.Forms.TextBox noStudInput;
+        private System.Windows.Forms.Label noAssignLabel;
+        private System.Windows.Forms.Label noStudentsLabel;
+        private System.Windows.Forms.Label testLabel;
+        private System.Windows.Forms.Button testButton;
+        private System.Windows.Forms.Label testLabel2;
+        private System.Windows.Forms.GroupBox studentInfoBox;
+        private System.Windows.Forms.Label studentNameLabel;
+        private System.Windows.Forms.Button saveNameButton;
+        private System.Windows.Forms.TextBox nameInput;
+        private System.Windows.Forms.GroupBox navBox;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button nextStudButton;
+        private System.Windows.Forms.Button prevStudButton;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Assignment3/Form1.cs
+++ Assignment3/Form1.cs
@@ -12,9 +12,60 @@ namespace Assignment3
 {
     public partial class Form1 : Form
     {
+        string[] students;
+        int[,] assignments;
+        int p = 0;
+
+
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            students = new string[Convert.ToInt32(noStudInput.Text)];
+            assignments = new int[Convert.ToInt32(noStudInput.Text), Convert.ToInt32(noAssignInput.Text)];
+
+            for (int i = 0; i < students.Length; i++)
+            {
+                students[i] = "Student #" + (i);
+            }
+
+            studentNameLabel.Text = students[p];
+        }
+
+        private void testButton_Click(object sender, EventArgs e)
+        {
+            testLabel.Text = students.Length.ToString();
+            testLabel2.Text = assignments.Length.ToString();
+        }
+
+        private void nextStudButton_Click(object sender, EventArgs e)
+        {
+            if (p < students.Length)
+            {
+                p++;
+                studentNameLabel.Text = students[p];
+            }
+            else
+            {
+                studentNameLabel.Text = students[p];
+            }
+        }
+
+        private void prevStudButton_Click(object sender, EventArgs e)
+        {
+            if (p > 0)
+            {
+                p--;
+                studentNameLabel.Text = students[p];
+            }
+        }
+
+
+
+
     }
 }
